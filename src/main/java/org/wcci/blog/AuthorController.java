65770adcab1c4a22f05ad33065badda4ebdf2461
JGibson37 <Jason.Gibson15@yahoo.com>
2020-06-30 21:52:34 -0400

    building author controller

--- src/main/java/org/wcci/blog/AuthorController.java
+++ src/main/java/org/wcci/blog/AuthorController.java
@@ -1,4 +1,27 @@
 package org.wcci.blog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AuthorController {
+
+    private AuthorStorage authorStorage;
+    private AuthorRepository authorRepo;
+    private BlogPostRepository blogRepo;
+
+    public AuthorController(AuthorStorage authorStorage, AuthorRepository authorRepo, BlogPostRepository blogRepo) {
+        this.authorStorage = authorStorage;
+        this.authorRepo = authorRepo;
+        this.blogRepo = blogRepo;
+    }
+
+    @RequestMapping("author/{name}")
+    public String displayAuthor(@PathVariable String name, Model model){
+        model.addAttribute(authorStorage.authorByName(name));
+        return "author-template";
+
+    }
 }
--- src/main/java/org/wcci/blog/BlogPostRepository.java
+++ src/main/java/org/wcci/blog/BlogPostRepository.java
@@ -2,7 +2,7 @@ package org.wcci.blog;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface BlogPostStorage extends CrudRepository<BlogPost, Long> {
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
 
     BlogPost blogPostByName(String name);
 }
